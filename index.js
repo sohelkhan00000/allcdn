d29e044f63ba6b923ddc9f08e3db8b2733b17529
sohel khan <sohelkhan00000@gmail.com>
2023-10-06 17:08:09 +0530

    vercel test

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname))
+
 
 app.get('/', async (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+    app.use(express.static(__dirname));
+    res.sendFile(__dirname + "/index.html");
 });
 app.listen(3000);
 module.exports = app
