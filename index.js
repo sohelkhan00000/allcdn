73aaa3ad35e93683b76e44ce1e9341df838491a0
sohel khan <sohelkhan00000@gmail.com>
2023-10-09 11:31:49 +0530

    favicon added

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
 
-
-
 app.get('/', async (req, res) => {
-    app.use(express.static(__dirname));
-    res.sendFile(__dirname + "/index.html");
+    app.use(express.static(__dirname + "/public"));
+    res.sendFile(__dirname + "/public/index.html");
 });
 app.listen(3000);
 module.exports = app
